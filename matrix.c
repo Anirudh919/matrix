57133daffe2772a7071a3cb8c0a5be82c8b1b481
Anirudh919 <101653588+Anirudh919@users.noreply.github.com>
2022-05-22 12:50:05 +0530

    change

--- matrix.c
+++ matrix.c
@@ -13,27 +13,27 @@ void multiplicationmatrix()
 
 int main()
 {
-    int n, i, j, b, a[10][10], m;
+    int n, i, j, b, a[10][10], m, c;
     printf("Enter the how many number of matrix insert:");
-    scanf("%d", &n);
-    for (b = 0; b < n; b++)
+    scanf("%d", &c);
+    for (b = 0; b < c; b++)
     {
-        printf("Enter the order of matrix:");
-        scanf("%d%d", &n, &m);
+        printf("Enter the number of column & row of matrix:");
+        scanf("%d%d", &m, &n);
         printf("Enter the matrix %d:", b + 1);
-        for (i = 0; i < n; i++)
+        for (i = 0; i < m; i++)
         {
             for (j = 0; j < n; j++)
             {
-                scanf("%d", &a[m][n]);
+                scanf("%d", &a[i][j]);
             }
         }
         printf("Matrix %d:\n", b + 1);
-        for (i = 0; i < n; i++)
+        for (i = 0; i < m; i++)
         {
-            for (j = 0; j < m; j++)
+            for (j = 0; j < n; j++)
             {
-                printf("%d\t", a[m][n]);
+                printf("%d\t", a[i][j]);
             }
             printf("\n");
         }
