297d2fa59de76f984c9e6bc8ff98e8ed211d0dc4
Anirudh919 <101653588+Anirudh919@users.noreply.github.com>
2022-05-22 16:36:30 +0530

    change

--- matrix.c
+++ matrix.c
@@ -1,8 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
-void addmatrix()
+void addmatrix(int a[][])
 {
+    int sum, m, n, i, j;
+    for (i = 0; i < m; i++)
+    {
+        for (j = 0; j < n; j++)
+        {
+            sum = a[i][j] + (++a[i][j]);
+        }
+    }
+    for (i = 0; i < m; i++)
+    {
+        for (j = 0; j < n; j++)
+        {
+            printf("Addition of matrix%d:\n", sum);
+        }
+    }
 }
 void subtractmatrix()
 {
@@ -13,7 +28,7 @@ void multiplicationmatrix()
 
 int main()
 {
-    int n, i, j, b, a[10][10], m, c;
+    int n, i, j, b, a[10][10], m, c, choice, d;
     printf("Enter the how many number of matrix insert:");
     scanf("%d", &c);
     for (b = 0; b < c; b++)
@@ -38,4 +53,28 @@ int main()
             printf("\n");
         }
     }
+    do
+    {
+        printf("\n1. Addition of matrix\n2. Subtraction of matrix\n3. Multiplication of matrix\n4. Exit");
+        scanf("%d", &choice);
+        switch (choice)
+        {
+        case 1:
+            addmatrix(a[i][j]);
+            break;
+        case 2:
+            subtractmatrix();
+            break;
+        case 3:
+            multiplicationmatrix();
+            break;
+        case 4:
+            exit(0);
+            break;
+
+        default:
+            printf("Enter the valid choice:");
+            break;
+        }
+    } while (d == 'y');
 }
